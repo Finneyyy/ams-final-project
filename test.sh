41211bc2db74b955eca298d2b03538367f4552f8
hivespaula <paula.hives@weareams.comm>
2023-01-18 18:24:40 +0000

    test sudo

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
-npm install -g ts-node typescript '@types/node'
+sudo npm install -g ts-node typescript '@types/node'
 ts-node spring-petclinic-angular-master/spring-petclinic-angular-master/src/test.ts
