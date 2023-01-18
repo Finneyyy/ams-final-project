c4d69196ab3ccce0576bf8a3d6fd021dd74115c0
hivespaula <paula.hives@weareams.comm>
2023-01-18 18:29:42 +0000

    install angular

--- test.sh
+++ test.sh
@@ -2,4 +2,5 @@
 curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
 sudo npm install -g ts-node typescript '@types/node'
+sudo npm i @angular/cli@11.2.11
 ts-node spring-petclinic-angular-master/spring-petclinic-angular-master/src/test.ts
