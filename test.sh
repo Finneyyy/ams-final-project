8bcc1445f36245b8e71f9c10e4c6311a85d5e05d
Eoin <eoin.finney@gmail.com>
2023-01-19 17:05:23 +0000

    Update test.sh

--- test.sh
+++ test.sh
@@ -4,6 +4,6 @@ sudo apt-get install -y nodejs
 sudo npm install -g ts-node typescript '@types/node'
 sudo npm i -g @angular/cli@11.2.11
 sudo npm i @angular-devkit/build-angular@0.1102.19
-cd spring-petclinic-angular-master/spring-petclinic-angular-master
+cd spring-petclinic-angular-master
 ng test
 ng e2e
