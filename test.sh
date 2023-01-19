19b33fdd1923e4a9a6f1bb418fadc929aec39734
hivespaula <paula.hives@weareams.comm>
2023-01-19 10:33:18 +0000

    change install order

--- test.sh
+++ test.sh
@@ -2,6 +2,6 @@
 curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
 sudo npm install -g ts-node typescript '@types/node'
-sudo npm install ng
 sudo npm i -g @angular/cli@11.2.11
+sudo npm install ng
 ts-node spring-petclinic-angular-master/spring-petclinic-angular-master/src/test.ts
