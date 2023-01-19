5812d68722fe1820667de15758a187a9b1712c0a
Eoin <eoin.finney@gmail.com>
2023-01-19 16:56:10 +0000

    Updated test to add tests
    Commited by Eoin

--- test.sh
+++ test.sh
@@ -3,5 +3,7 @@ curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash - &&\
 sudo apt-get install -y nodejs
 sudo npm install -g ts-node typescript '@types/node'
 sudo npm i -g @angular/cli@11.2.11
-sudo npm install ng
-ts-node spring-petclinic-angular-master/spring-petclinic-angular-master/src/test.ts
+sudo npm i @angular-devkit/build-angular@0.1102.19
+cd spring-petclinic-angular-master
+ng test
+ng e2e
