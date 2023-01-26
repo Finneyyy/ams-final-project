1ee985bb7947c617621b5c5a5e1383dfc4bd08fb
finneyyy <eoin.finney@gmail.com>
2023-01-26 22:44:54 +0000

    fixed inventory.tf // commit eoin

--- terraform/inventory.tf
+++ terraform/inventory.tf
@@ -1,11 +1,7 @@
-data "template_file" "inventory" {
-  template = file("./templates/inventory.tpl")
-  vars = {
-    "web-server" = aws_instance.web-server.public_ip
-  }
-}
-
 resource "local_file" "inventory_file" {
-  content = data.template_file.inventory.rendered
   filename = "inventory.yaml"
+  content = <<EOF
+  [web-server]
+  ${aws_instance.web-server.public_ip}
+  EOF
 } 
